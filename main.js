978c1a1813f578a5f556b67db2486e1ad932add1
Anuj Yadav <anuj821yadav@gmail.com>
2025-12-10 08:02:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let a=document.querySelector('#btn1')
+a.addEventListener('click',function(){document.body.style.backgroundColor="green";})
+
+
+let b=document.querySelector('#btn2')
+b.addEventListener('click',function(){document.body.style.backgroundColor="teal";})
+
+
+let c=document.querySelector('#btn3')
+c.addEventListener('click',function(){document.body.style.backgroundColor="lightgreen";})
